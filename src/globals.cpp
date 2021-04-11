d0fae0e707c5b103701b5623669244e4958f8dca
southoz <adriangrant@iinet.net.au>
2021-04-11 17:35:25 +1000

    Update globals.cpp

--- src/globals.cpp
+++ src/globals.cpp
@@ -3,5 +3,5 @@
 
 RETRORUN_CORE_TYPE Retrorun_Core = RETRORUN_CORE_UNKNOWN;
 bool Retrorun_UseAnalogStick = false;
-
-bool opt_triggers = false;
+bool opt_triggers_right = false;
+bool opt_triggers_left = false;
